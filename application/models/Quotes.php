c4c7824f8e7436244f9ff5b4157a834e28e813e0
Jason Burns <admiralmudkip@gmail.com>
2016-10-08 17:59:55 -0700

    data created

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -8,22 +8,37 @@
  * @author jim
  */
 class Quotes extends CI_Model {
-
-	// The data comes from http://www.quotery.com/top-100-funny-quotes-of-all-time/?PageSpeed=noscript
-	var $data = array(
-		array('id' => '1', 'who' => 'Bob Monkhouse', 'mug' => 'bob-monkhouse-150x150.jpg', 'where' => '/sleep',
-			'what' => 'When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.'),
-		array('id' => '2', 'who' => 'Elayne Boosler', 'mug' => 'elayne-boosler-150x150.jpg', 'where' => '/lock/em/up',
-			'what' => 'I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.'),
-		array('id' => '3', 'who' => 'Mark Russell', 'mug' => 'mark-russell-150x150.jpg', 'where' => '/show/3',
-			'what' => 'The scientific theory I like best is that the rings of Saturn are composed entirely of lost airline luggage.'),
-		array('id' => '4', 'who' => 'Anonymous', 'mug' => 'Anonymous-150x150.jpg', 'where' => '/dunno',
-			'what' => 'How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell “BINGO!”'),
-		array('id' => '5', 'who' => 'Socrates', 'mug' => 'socrates-150x150.jpg', 'where' => '/wise/bingo',
-			'what' => 'By all means, marry. If you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.'),
-		array('id' => '6', 'who' => 'Isaac Asimov', 'mug' => 'isaac-asimov-150x150.jpg', 'where' => '/comp4711/wisdom',
-			'what' => 'Those people who think they know everything are a great annoyance to those of us who do.')
-	);
+    
+    var $supplies = array(
+        array('id' => 1, 'name' => 'Egg', 'mug' => '', 'recieving unit' => 10, 'cost' => 2.00, 'stocking unit' => 12, 'on hand' => 0),
+        array('id' => 2, 'name' => 'Sausage', 'mug' => '', 'recieving unit' => 10, 'cost' => 3.00, 'stocking unit' => 10, 'on hand' => 0),
+        array('id' => 3, 'name' => 'Bagel', 'mug' => '', 'recieving unit' => 20, 'cost' => 2.00, 'stocking unit' => 6, 'on hand' => 0),
+        array('id' => 4, 'name' => 'Bacon', 'mug' => '', 'recieving unit' => 15, 'cost' => 3.00, 'stocking unit' => 12, 'on hand' => 0),
+        array('id' => 5, 'name' => 'Cheese', 'mug' => '', 'recieving unit' => 40, 'cost' => 7.00, 'stocking unit' => 10, 'on hand' => 0),
+        array('id' => 6, 'name' => 'Hash Brown', 'mug' => '', 'recieving unit' => 20, 'cost' => 5.00, 'stocking unit' => 12, 'on hand' => 0),
+        array('id' => 7, 'name' => 'Coffee Beans', 'mug' => '', 'recieving unit' => 10, 'cost' => 10.00, 'stocking unit' => 5000, 'on hand' => 0),
+        array('id' => 8, 'name' => 'English Muffin', 'mug' => '', 'recieving unit' => 10, 'cost' => 5.00, 'stocking unit' => 6, 'on hand' => 0),
+        array('id' => 9, 'name' => 'Tortilla', 'mug' => '', 'recieving unit' => 20, 'cost' => 4.00, 'stocking unit' => 12, 'on hand' => 0),
+        array('id' => 10, 'name' => 'Tomato', 'mug' => '', 'recieving unit' => 20, 'cost' => 0.50, 'stocking unit' => 10, 'on hand' => 0),
+        array('id' => 11, 'name' => 'Lettuce', 'mug' => '', 'recieving unit' => 20, 'cost' => 2.00, 'stocking unit' => 20, 'on hand' => 0),
+        array('id' => 12, 'name' => 'Canadian Bacon', 'mug' => '', 'recieving unit' => 20, 'cost' => 5.00, 'stocking unit' => 20, 'on hand' => 0)
+    );
+    
+    var $recepies = array(
+        array('id' => 1, 'description' => 'Egg, Cheese, English Muffin, Canadian Bacon', 'ingredients' => array(1, 5, 8, 12)),
+        array('id' => 2, 'description' => 'Egg, Cheese, English Muffin, Sausage', 'ingredients' => array(1, 2, 5, 8),
+        array('id' => 3, 'description' => 'Bagel, Bacon, Tomato, Lettuce', 'ingredients' => array(3, 4, 10, 11),
+        array('id' => 4, 'description' => 'Totrilla, Sausage, Cheese, Hash Brown, Egg', 'ingredients' => array(9, 2, 5, 6, 1),
+		array('id' => 5, 'description' => 'Coffee', 'ingredients' => array(7)),
+    );
+    
+    var $stock = array(
+        array('id' => 1, 'name' => 'Egg McMuffin', 'price' => 3.50, 'quantity' => 0),
+        array('id' => 2, 'name' => 'Sausage McMuffin', 'price' => 4.00, 'quantity' => 0),
+        array('id' => 3, 'name' => 'Bagel BLT', 'price' => 5.00, 'quantity' => 0),
+        array('id' => 4, 'name' => 'Sausage & Hash Brown Breakfast Wrap', 'price' => 5.50, 'quantity' => 0),
+        array('id' => 5, 'name' => 'Coffee', 'price' => 2.00, 'quantity' => 0),
+    );
 
 	// Constructor
 	public function __construct()
