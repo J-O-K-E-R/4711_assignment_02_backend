404163eb35ae53c20bdf18293027a8c07c31b46f
limbermuffin <limbermuffin@gmail.com>
2016-10-09 19:53:51 -0700

    Added logging for items recieved through recieving page

--- application/models/Supplies.php
+++ application/models/Supplies.php
@@ -10,18 +10,18 @@
 class Supplies extends CI_Model {
 
 	var $supplies = array(
-        array('id' => 1, 'name' => 'Egg', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 2, 'name' => 'Sausage', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 3, 'name' => 'Bagel', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 4, 'name' => 'Bacon', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 5, 'name' => 'Cheese', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 6, 'name' => 'Hash Brown', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 7, 'name' => 'Coffee Beans', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 8, 'name' => 'English Muffin', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 9, 'name' => 'Tortilla', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 10, 'name' => 'Tomato', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 11, 'name' => 'Lettuce', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
-        array('id' => 12, 'name' => 'Canadian Bacon', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5);
+        array('id' => 1, 'name' => 'Egg', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 2, 'name' => 'Sausage', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 3, 'name' => 'Bagel', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 4, 'name' => 'Bacon', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 5, 'name' => 'Cheese', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 6, 'name' => 'Hash Brown', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 7, 'name' => 'Coffee Beans', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 8, 'name' => 'English Muffin', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 9, 'name' => 'Tortilla', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 10, 'name' => 'Tomato', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 11, 'name' => 'Lettuce', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5),
+        array('id' => 12, 'name' => 'Canadian Bacon', 'on hand' => 0, 'containers per shipment' => 10, 'containers' => 0, 'items per container' => 10, 'cost' => 5)
     );
     
     public function orderSupplies($itemID){
--- application/views/recieving.php
+++ application/views/recieving.php
@@ -1,10 +1,14 @@
 <div class="row">
     <div class="span8 offset1">
-        <h2>Supplies</h2>
-        <div class="row">
-            {supplies}
-            <div class="span4"><b>{name}</b>: {on hand}</div>
-            {/supplies}
-        </div>
+        <h2>Supplies to recieve</h2>
+        <form action="recieving" method="POST">
+            <div class="row">
+                {supplies}
+                <div class="span4"><b>{name} x{stocking unit}:</b></div>
+                <input type="number" name="{name}" value="0" min="0" max="99">
+                {/supplies}
+            </div>
+            <input type="submit" value="Submit">
+        </form>
     </div>
 </div>
