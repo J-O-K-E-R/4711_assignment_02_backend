f1cedfbcb437e0df5cb4cd59ad35bcdc0f84825d
Jason Burns <admiralmudkip@gmail.com>
2016-10-09 18:47:40 -0700

    back end stuff, editing stock

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,5 +1,22 @@
 <div class="row">
     <div class="span8 offset1">
-        Test homepage
+        Purchases: 
+    </div>
+</div>
+
+<div class = "row">
+    <div class="span8 offset1">
+        Sales:
+    </div>
+</div>
+
+<div class = "row">
+    <div class="span8 offset1">
+        Cost of sales:
+    </div>
+</div>
+<div class = "row">
+    <div class="span8 offset1">
+        Ingredients consumed:
     </div>
 </div>
