eef17e867d1a29ff68e1db90581c601d306ec6d0
limbermuffin <limbermuffin@gmail.com>
2016-12-11 17:27:28 -0800

    Modified aministrator page, added placeholder functions

--- application/views/administrator.php
+++ application/views/administrator.php
@@ -1,31 +1,78 @@
 <!-- gross.  Organized into tables for easy layouts. -->
 
 <div class="span10 offset1">
+    <h4>Recipes</h4>
     <table>
-    {recipesRow}
-    <tr>
-    <td>{name}</td><td>{description}&nbsp;</td><td><input type="submit" value="edit"></td><td><input type="submit" value="delete"></td>
-    </tr>
-    {/recipesRow}
+        {recipesRow}
+        <tr>
+            <td>
+                {name}
+            </td>
+            <td>
+                {description}&nbsp;
+            </td>
+            <td>
+                <a class='btn btn-danger' role="button" href="/administrator/delete_recipe/{id}/">Delete</a>
+            </td>
+        </tr>
+        {/recipesRow}
     </table>
-    <input type="submit" value="Add">
+    <a class="btn btn-default" role="button" href="/administrator/add_recipe/">Add</a>
     <br><br>
+    <h4>Stock</h4>
     <table>
-    {stockRow}
-    <tr>
-    <td>{name}&nbsp;</td><td>Price: {price}&nbsp;</td><td>Quantity: {quantity}&nbsp;&nbsp;</td><td><input type="submit" value="edit"></td><td><input type="submit" value="delete"></td>
-    </tr>
-    {/stockRow}
+        {stockRow}
+        <tr>
+            <td>
+                {name}&nbsp;
+            </td>
+            <td>
+                Price: {price}&nbsp;
+            </td>
+            <td>
+                Quantity: {quantity}&nbsp;&nbsp;
+            </td>
+            <td>
+                <a class="btn btn-default" role="button" href="/administrator/edit_stock/{id}/">Edit</a>
+            </td>
+            <td>
+                <a class='btn btn-danger' role="button" href="/administrator/delete_stock/{id}/">Delete</a>
+            </td>
+        </tr>
+        {/stockRow}
     </table>
-    <input type="submit" value="Add">
+    <a class="btn btn-default" role="button" href="/administrator/add_stock/">Add</a>
     <br><br>
+    <h4>Supplies</h4>
     <table>
-    {suppliesRow}
-    <tr>
-    <td>{name}</td><td>On hand:{on hand}</td><td>Containers/shipment: {containerspership}&nbsp;</td><td>Containers: {containers}&nbsp;</td><td>Items/container: {itemspercontainer}&nbsp;</td><td>Cost: {cost}&nbsp;&nbsp;</td><td><input type="submit" value="edit"></td><td><input type="submit" value="delete"></td>
-    </tr>
-    {/suppliesRow}
+        {suppliesRow}
+        <tr>
+            <td>
+                {name}
+            </td>
+            <td>
+                On hand:{on hand}
+            </td>
+            <td>
+                Containers/shipment: {containerspership}&nbsp;
+            </td>
+            <td>
+                Containers: {containers}&nbsp;
+            </td>
+            <td>
+                Items/container: {itemspercontainer}&nbsp;
+            </td>
+            <td>
+                Cost: {cost}&nbsp;&nbsp;
+            </td>
+            <td>
+                <a class="btn btn-default" role="button" href="/administrator/edit_supply/{id}/">Edit</a>
+            </td>
+            <td>
+            <a class='btn btn-danger' role="button" href="/administrator/delete_supply/{id}/">Delete</a>
+        </tr>
+        {/suppliesRow}
     </table>
-    <input type="submit" value="Add">
+    <a class="btn btn-default" role="button" href="/administrator/add_supply/">Add</a>
     <br><br>
 </div>
